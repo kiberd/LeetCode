3a3f1f5df54c11486097bb2f3a80d7a1833fd6d9
kiberd <34852597+kiberd@users.noreply.github.com>
2021-12-12 12:13:50 +0900

    Time: 104 ms (45.14%), Space: 39.9 MB (80.20%) - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    const answer = haystack.indexOf(needle);
+    
+    return answer
+};
