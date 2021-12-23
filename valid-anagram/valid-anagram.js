046d42076054a1c7b9955bd6db25d5291d0f9a89
kiberd <34852597+kiberd@users.noreply.github.com>
2021-12-23 13:07:00 +0900

    Time: 108 ms (37.39%), Space: 44.9 MB (14.08%) - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    return ( JSON.stringify(s.split('').sort()) === JSON.stringify(t.split('').sort()) );
+    
+};
