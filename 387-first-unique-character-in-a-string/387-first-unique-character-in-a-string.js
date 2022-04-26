ffd155fde52cb57c9c17f05145ed95251374cd9a
kiberd <34852597+kiberd@users.noreply.github.com>
2022-04-26 21:36:05 +0900

    Time: 114 ms (75.18%), Space: 47.6 MB (28.22%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -6,16 +6,26 @@
 
 var firstUniqChar = function(s) {
     
+//     // First solution
+//     const wordArry = s.split('');
+//     const wordCountObj = {};
+    
+//     wordArry.map((word) => {
+//         if (wordCountObj[word]) wordCountObj[word]++;
+//         else wordCountObj[word] = 1;
+//     })
+    
+//     for (let i = 0; i < s.length; i++){
+//         if (wordCountObj[s[i]] === 1) return i;
+//     }
+    
+//     return -1;
+    
+    // Second solution
     const wordArry = s.split('');
-    const wordCountObj = {};
     
-    wordArry.map((word) => {
-        if (wordCountObj[word]) wordCountObj[word]++;
-        else wordCountObj[word] = 1;
-    })
-    
-    for (let i = 0; i < s.length; i++){
-        if (wordCountObj[s[i]] === 1) return i;
+    for (let i = 0; i < wordArry.length; i++){
+        if (s.indexOf(wordArry[i]) === s.lastIndexOf(wordArry[i])) return i;
     }
     
     return -1;
